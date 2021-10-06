07fdbaffc44d2fce3ccc75c56ee6ddccc9b0bd77
VWGomes <88067055+VWGomes@users.noreply.github.com>
2021-10-06 16:27:01 -0300

    Operações matemáticas - número 3

---
+++ Operações matemáticas - número 3.py
@@ -0,0 +1,18 @@
+#Soma
+print("Ao somar o número 3 por 3, temos:")
+print("A soma de 3 é: %i" % (3 + 3))
+#Soma
+print("Ao subtrair o número 3 por 3, temos:")
+print("A subração de 3 por 3 é: %i" % (3 - 3))
+#Multiplicação
+print("Ao multiplicar o número 3 por 3, temos:")
+print("A multiplicação de 3 por 3é: %i" % (3 * 3))
+#Divisão
+print("Ao dividir o número 3 por 3, temos:")
+print("A divisão de 3 por 3 é: %i" % (3 / 3))
+#Potência
+print("Ao calcular a potência do número 3³, temos:")
+print("A potência de 3 é: %i" % (3 ** 3))
+#Módulo
+print("Ao calcular o módulo do número 3 por 2, temos:")
+print("O módulo de 3 por 2 é: %i" % (3 % 2))
